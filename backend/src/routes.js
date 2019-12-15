45ff8969abf246c3cac3e8c33b744feefc86ad63
Joao Lucas <joao.frhb@gmail.com>
2019-12-14 22:07:10 -0300

    Subindo projeto

---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
+const express = require('express');
+const multer = require('multer');
+const mongoose = require('mongoose');
+const uploadConfig = require('./config/upload');
+
+const routes = new express.Router();
+const upload = multer(uploadConfig);
+
+routes.get('/posts', PostController.index);
+routes.post('/posts', upload.single('image'), PostController.store);
+routes.post('/posts/:id/like', LikeController.store);
+
+module.exports = routes;
